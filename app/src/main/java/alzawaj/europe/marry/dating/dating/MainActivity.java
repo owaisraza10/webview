34667cbd5dfe5f1c77cd4dee816968ae8891dc70
Owais Raza <owaishraza53@gmail.com>
2023-05-28 20:15:19 +0530

    second commit

--- app/src/main/java/alzawaj/europe/marry/dating/dating/MainActivity.java
+++ app/src/main/java/alzawaj/europe/marry/dating/dating/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +15,8 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     NewsFragment newsFragment = new NewsFragment();
     ServicesFragment servicesFragment = new ServicesFragment();
     VoiceFragment voiceFragment = new VoiceFragment();
+    RssFragment rssFragment = new RssFragment();
 
 
 
@@ -92,9 +97,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-            case R.id.settings:
-                Toast.makeText(this, "Settings Is Selected", Toast.LENGTH_SHORT).show();
+            case R.id.english:
+                showLanguageSelectionDialogen();
+                return true;
+            case R.id.fr:
+                showLanguageSelectionDialog();
+                return true;
+        }
+
+        switch (item.getItemId()){
+            case R.id.rss:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,rssFragment).commit();
         }
         return super.onOptionsItemSelected(item);
     }
+     private void showLanguageSelectionDialog() {
+         String selectedLanguage = "fr";
+         changeLanguage(selectedLanguage);
+     }
+    private void showLanguageSelectionDialogen() {
+        String selectedLanguage = "eng";
+        changeLanguage(selectedLanguage);
+    }
+    private void changeLanguage(String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+
+        Resources resources = getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+
+        // Restart the activity or recreate relevant views
+        recreate();
+    }
 }
+
+
